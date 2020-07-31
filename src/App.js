dac6edcc7f94cfdddb2f927cb74424055d8191c2
deeppaz <deeppaz@hotmail.com>
2020-07-31 18:18:38 +0300

    update

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Banner from './components/Header/Banner';
 
+//body
+import PortfolioBar from './components/Body/PortfolioBar';
+import Portfolio from './components/Body/Portfolio';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
+      <PortfolioBar />
+      <Portfolio />
     </div>
   );
 }
