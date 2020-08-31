7816ea870ead29a85674bd6a9af5f215918940b2
deeppaz <deeppaz@hotmail.com>
2020-08-31 13:46:57 +0300

    update

--- src/store.js
+++ src/store.js
@@ -18,39 +18,39 @@ const state = {
       offset: 2,
       factor: 2.0,
       header: "2",
-      image: "image/se-11.png",
+      image: "image/se-12.png",
       aspect: 1.5,
       text:
-        "The man who comes back through the Door in the Wall will never be quite the same as the man who went out. He will be wiser but less sure, happier but less self-satisfied, humbler in acknowledging his ignorance yet better equipped to understand the relationship of words to things, of systematic reasoning to the unfathomable mystery which it tries, forever vainly, to comprehend."
+        "Packaging design is the process of designing product packaging to securely contain, identify and deliver a product"
     },
     {
       offset: 3,
       factor: 2.25,
       header: "3",
-      image: "image/se-11.png",
+      image: "image/se-13.png",
       aspect: 1.5037,
       text:
-        "The substance can take you to heaven but it can also take you to hell. Or else to both, together or alternately. Or else (if you're lucky, or if you've made yourself ready) beyond either of them. And then beyond the beyond, back to where you started from — back to here, back to New Rotham sted, back to business as usual. Only now, of course, business as usual is completely different."
+        "Packaging design is the process of designing product packaging to securely contain, identify and deliver a product"
     },
     {
       offset: 4,
       factor: 2.0,
       header: "4",
-      image: "image/se-11.png",
+      image: "image/se-04.png",
       aspect: 0.665,
       text:
-        "We’ve found that the people whose EEG doesn’t show any alpha-wave activity when they’re relaxed aren’t likely to respond significantly to the substance. That means that, for about fifteen percent of the population, we have to find other approaches to liberation."
+        "Instagram Post"
     },
     {
       offset: 5,
       factor: 1.75,
       header: "5",
-      image: "image/se-11.png",
+      image: "image/se-09.png",
       aspect: 1.55,
       text:
-        "By cultivating the state of mind that makes it possible for the dazzling ecstatic insights to become permanent and habitual illuminations. By getting to know oneself to the point where one won’t be compelled by one’s unconscious to do all the ugly, absurd, self-stultifying things that one so often finds oneself doing."
+        "Mobile Design."
     },
-    { offset: 7, factor: 1.05, header: "6", image: "image/se-11.png", aspect: 1.77, text: "Education and enlightenment." }
+    { offset: 7, factor: 1.05, header: "6", image: "image/se-08.png", aspect: 1.77, text: "Web Design." }
   ],
   stripes: [
     { offset: 0, color: "#000", height: 13 },
